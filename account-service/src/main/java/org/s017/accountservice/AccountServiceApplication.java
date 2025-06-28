a11fb6421b276c055c1318756115c4808b5fd13a
Thomas Villeroy <thomasvilleroy@outlook.com>
2025-06-28 00:18:26 -0300

    Add mongo config and ports config

--- account-service/src/main/java/org/s017/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/org/s017/accountservice/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package org.s017.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories()
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
--- borrowing-service/src/main/java/org/s017/borrowingservice/BorrowingServiceApplication.java
+++ borrowing-service/src/main/java/org/s017/borrowingservice/BorrowingServiceApplication.java
@@ -2,8 +2,10 @@ package org.s017.borrowingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories()
 public class BorrowingServiceApplication {
 
     public static void main(String[] args) {
